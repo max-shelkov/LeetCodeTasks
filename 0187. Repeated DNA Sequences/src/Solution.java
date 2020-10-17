29ac7a6063599fd67d3b10540c3ba3180b699398
max shelkov <44141938+max-shelkov@users.noreply.github.com>
2020-10-17 20:42:27 +0300

    0187. Repeated DNA Sequences done

--- 0187. Repeated DNA Sequences/src/Solution.java
+++ 0187. Repeated DNA Sequences/src/Solution.java
@@ -1,6 +1,9 @@
 import java.util.*;
 
 public class Solution {
+
+    //done
+
     public static void main(String[] args) {
         String inputS = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT";
         Solution solution = new Solution();
