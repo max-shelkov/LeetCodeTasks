020cd8415994288ed73c0ba3bbc80bdd75c60836
max shelkov <44141938+max-shelkov@users.noreply.github.com>
2020-10-17 20:41:24 +0300

    0074. Search a 2D Matrix done

---
+++ 0187. Repeated DNA Sequences/src/Solution.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        String inputS = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT";
+        Solution solution = new Solution();
+        List<String> res = null;
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 100; i++) {
+            res = solution.findRepeatedDnaSequences(inputS);
+        }
+        long finish = System.currentTimeMillis();
+        System.out.println("res = " + res);
+        System.out.println("time = " + (finish - start));
+    }
+
+    public List<String> findRepeatedDnaSequences(String s) {
+        List<String> res = new ArrayList<>();
+        if (s == null || s.length() < 11) return res;
+        int sequenceLength = 10;
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < s.length()-sequenceLength+1; i++) {
+            String sub = s.substring(i, i+sequenceLength);
+            if (!set.add(sub)) {
+                if (!res.contains(sub)) res.add(sub);
+            }
+        }
+        return res;
+    }
+/*
+    public List<String> findRepeatedDnaSequences(String s) {
+        List<String> res = new ArrayList<>();
+        if (s == null || s.length() < 11) return res;
+        int sequenceLength = 10;
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length()-sequenceLength+1; i++) {
+            String sub = s.substring(i, i+sequenceLength);
+            if (map.containsKey(sub)) {
+                map.replace(sub, map.get(sub)+1);
+                if (map.get(sub) == 2) res.add(sub);
+            } else{
+                map.put(sub,1);
+            }
+        }
+        return res;
+    }
+*/
+
+}
