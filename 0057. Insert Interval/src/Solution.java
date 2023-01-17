0f0e9072a28e6802726ff00858c958fd42707e97
max-shelkov <dEEp4t0wnBUH>
2023-01-17 08:37:11 +0300

    0057. Insert Interval done

--- 0057. Insert Interval/src/Solution.java
+++ 0057. Insert Interval/src/Solution.java
@@ -1,15 +1,12 @@
 import java.util.ArrayList;
 import java.util.Comparator;
+
 //done
 class Solution {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-//        int[][] input = {{1,3},{6,9}};
-//        int[] newInterval = {2, 5};
-//        int[][] input = {{1, 2}, {3, 5}, {6, 7}, {8, 10}, {12, 16}};
-//        int[] newInterval = {4, 8};
-        int[][] input = {{3,5},{12,15}};
+        int[][] input = {{3, 5}, {12, 15}};
         int[] newInterval = {6, 6};
         long start = System.currentTimeMillis();
         int[][] res = solution.insert(input, newInterval);
@@ -28,7 +25,7 @@ class Solution {
 
 
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        if (intervals.length == 0) return new int[][] {newInterval};
+        if (intervals.length == 0) return new int[][]{newInterval};
 
 
         ArrayList<int[]> res = new ArrayList<>();
@@ -56,32 +53,14 @@ class Solution {
         int mergedBeginNum = numOfFirstIntervalToMerge >= 0 ? Math.min(newInterval[0], intervals[numOfFirstIntervalToMerge][0]) : newInterval[0];
         int mergedEndNum = numOfLastIntervalToMerge >= 0 ? Math.max(newInterval[1], intervals[numOfLastIntervalToMerge][1]) : newInterval[1];
 
-        int[] merged = {mergedBeginNum,mergedEndNum};
-        boolean mergedAdded = false;
+        int[] merged = {mergedBeginNum, mergedEndNum};
 
 
-
-        for (int i = 0; i < intervals.length; i++) {
-            if (intervals[i][0] >= merged[0] && intervals[i][0] <= merged[1]) {
-//                if (!mergedAdded) {
-//                    res.add(merged);
-//                    mergedAdded = true;
-//                }
-                continue;
-            }
-
-            res.add(intervals[i]);
+        for (int[] item : intervals) {
+            if (item[0] >= merged[0] && item[0] <= merged[1]) continue;
+            res.add(item);
         }
 
-/*
-        if (!mergedAdded){
-            if (merged[1] < res.get(0)[0]) {
-                res.add(0,merged);
-            } else {
-                res.add(merged);
-            }
-        }
-*/
         res.add(merged);
         res.sort(Comparator.comparingInt(o -> o[0]));
 
@@ -91,14 +70,8 @@ class Solution {
         }
 
 
-
-
-//        System.out.println("numOfFirstIntervalToMerge = " + numOfFirstIntervalToMerge);
-//        System.out.println("numOfLastIntervalToMerge = " + numOfLastIntervalToMerge);
-//        System.out.println("result = " + result);
         return result;
     }
-
 
 
 }
